ccebd0a8d9c446aa228f7566de23ba8ac548729a
Gabriel Azevedo <gabrielalmeida75@gmail.com>
2025-05-31 16:28:13 -0300

    feat: add name field to lazydev provider config

--- init.lua
+++ init.lua
@@ -907,7 +907,7 @@ require('lazy').setup({
       sources = {
         default = { 'lsp', 'path', 'snippets', 'lazydev' },
         providers = {
-          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          lazydev = { name = 'lazydev', module = 'lazydev.integrations.blink', score_offset = 100 },
         },
       },
 
